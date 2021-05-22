580d47053b45cbe11b88820ad540a9526622603e
sakura-rip <56917703+sakura-rip@users.noreply.github.com>
2021-05-22 19:25:35 +0900

    add: create vps api

--- vps.go
+++ vps.go
@@ -32,3 +32,45 @@ func (c *Conoha) GetVPSList() ([]GetVPSListResponseServer, error) {
 	}
 	return res.Servers, nil
 }
+
+type CreateVPSRequest struct {
+	Server CreateVPSRequestServer `json:"server"`
+}
+type CreateVPSRequestServer struct {
+	Imageref  string `json:"imageRef"`
+	Flavorref string `json:"flavorRef"`
+	Adminpass string `json:"adminPass"`
+	KeyName   string `json:"key_name"`
+}
+
+type CreateVPSResponse struct {
+	Server struct {
+		OsDcfDiskconfig string `json:"OS-DCF:diskConfig"`
+		Adminpass       string `json:"adminPass"`
+		ID              string `json:"id"`
+		Links           []struct {
+			Href string `json:"href"`
+			Rel  string `json:"rel"`
+		} `json:"links"`
+		SecurityGroups []struct {
+			Name string `json:"name"`
+		} `json:"security_groups"`
+	} `json:"server"`
+}
+
+func (c *Conoha) CreateVPS(imageRef, flovorRef, adminPassword, sshKeyName string) (string, error) {
+	data := req.BodyJSON(CreateVPSRequest{Server: CreateVPSRequestServer{imageRef, flovorRef, adminPassword, sshKeyName}})
+	r, err := req.Post(c.endPoint.ToUrl(ComputeService, "servers"), data)
+	if err != nil {
+		return "", err
+	}
+	res := CreateVPSResponse{}
+	if r.Response().StatusCode != 200 {
+		return "", xerrors.Errorf("wrong status code: %v, message: %v", r.Response().StatusCode, r.String())
+	}
+	err = r.ToJSON(&res)
+	if err != nil {
+		return "", err
+	}
+	return res.Server.ID, nil
+}
