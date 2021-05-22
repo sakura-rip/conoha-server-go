572ed6bdd4814e0c5c896aca1a0ad74a29458718
sakura-rip <56917703+sakura-rip@users.noreply.github.com>
2021-05-22 18:45:08 +0900

    change: to return error

--- client.go
+++ client.go
@@ -1,16 +1,18 @@
 package conoha_server_go
 
+import "golang.org/x/xerrors"
+
 type Conoha struct {
 	header   map[string]string
 	endPoint *EndPoint
 }
 
-func NewConohaClient(userName, password, tenantId string) *Conoha {
+func NewConohaClient(userName, password, tenantId string) (*Conoha, error) {
 	cl := &Conoha{endPoint: NewEndPoint(tenantId), header: map[string]string{}}
 	token, err := cl.issueApiToken(userName, password, tenantId)
 	if err != nil {
-		panic(err)
+		return nil, xerrors.Errorf("failed create conoha client: %w", err)
 	}
 	cl.header["X-Auth-Token"] = token
-	return cl
+	return cl, nil
 }
