99d79c031914ca8208ade4c5d38ceed6dca94a97
sakura-rip <56917703+sakura-rip@users.noreply.github.com>
2021-05-22 19:25:08 +0900

    change: use inline type def

--- token.go
+++ token.go
@@ -19,53 +19,44 @@ type Auth struct {
 	TenantId            string              `json:"tenantId"`
 }
 
-// GetTokenResponse response
-type GetTokenResponse struct {
-	Access Access `json:"access"`
-}
-type Tenant struct {
-	DomainID    string `json:"domain_id"`
-	Description string `json:"description"`
-	Enabled     bool   `json:"enabled"`
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-}
-type Token struct {
-	IssuedAt string    `json:"issued_at"`
-	Expires  time.Time `json:"expires"`
-	ID       string    `json:"id"`
-	Tenant   Tenant    `json:"tenant"`
-	AuditIds []string  `json:"audit_ids"`
-}
-type Endpoints struct {
-	Region    string `json:"region"`
-	PublicUrl string `json:"publicURL"`
-}
-type ServiceCatalog struct {
-	Endpoints      []Endpoints   `json:"endpoints"`
-	EndpointsLinks []interface{} `json:"endpoints_links"`
-	Type           string        `json:"type"`
-	Name           string        `json:"name"`
-}
-type Roles struct {
-	Name string `json:"name"`
-}
-type User struct {
-	Username   string        `json:"username"`
-	RolesLinks []interface{} `json:"roles_links"`
-	ID         string        `json:"id"`
-	Roles      []Roles       `json:"roles"`
-	Name       string        `json:"name"`
-}
-type Metadata struct {
-	IsAdmin int      `json:"is_admin"`
-	Roles   []string `json:"roles"`
-}
-type Access struct {
-	Token          Token            `json:"token"`
-	ServiceCatalog []ServiceCatalog `json:"serviceCatalog"`
-	User           User             `json:"user"`
-	Metadata       Metadata         `json:"metadata"`
+type IssueApiTokenResponse struct {
+	Access struct {
+		Token struct {
+			IssuedAt string    `json:"issued_at"`
+			Expires  time.Time `json:"expires"`
+			ID       string    `json:"id"`
+			Tenant   struct {
+				DomainID    string `json:"domain_id"`
+				Description string `json:"description"`
+				Enabled     bool   `json:"enabled"`
+				ID          string `json:"id"`
+				Name        string `json:"name"`
+			} `json:"tenant"`
+			AuditIds []string `json:"audit_ids"`
+		} `json:"token"`
+		Servicecatalog []struct {
+			Endpoints []struct {
+				Region    string `json:"region"`
+				Publicurl string `json:"publicURL"`
+			} `json:"endpoints"`
+			EndpointsLinks []interface{} `json:"endpoints_links"`
+			Type           string        `json:"type"`
+			Name           string        `json:"name"`
+		} `json:"serviceCatalog"`
+		User struct {
+			Username   string        `json:"username"`
+			RolesLinks []interface{} `json:"roles_links"`
+			ID         string        `json:"id"`
+			Roles      []struct {
+				Name string `json:"name"`
+			} `json:"roles"`
+			Name string `json:"name"`
+		} `json:"user"`
+		Metadata struct {
+			IsAdmin int      `json:"is_admin"`
+			Roles   []string `json:"roles"`
+		} `json:"metadata"`
+	} `json:"access"`
 }
 
 func (c *Conoha) issueApiToken(userName, password, tenantId string) (string, error) {
@@ -83,7 +74,7 @@ func (c *Conoha) issueApiToken(userName, password, tenantId string) (string, err
 	if r.Response().StatusCode != 200 {
 		return "", xerrors.Errorf("wrong status code: %v, message: %v", r.Response().StatusCode, r.String())
 	}
-	var res GetTokenResponse
+	var res IssueApiTokenResponse
 	err = r.ToJSON(&res)
 	if err != nil {
 		return "", xerrors.Errorf("failed get token: %w", err)
