fbc40d7e751a00170997c1f2b7ff9a6f25636d6d
sakura-rip <56917703+sakura-rip@users.noreply.github.com>
2021-05-23 10:52:17 +0900

    add: add port func

--- port.go
+++ port.go
@@ -43,3 +43,35 @@ func (c *Conoha) GetPortList() ([]Port, error) {
 	}
 	return res.Ports, nil
 }
+
+type AddPortRequest struct {
+	Port AddPortRequestPort `json:"port"`
+}
+type AddPortRequestFixedIps struct {
+	IPAddress string `json:"ip_address"`
+	SubnetID  string `json:"subnet_id"`
+}
+type AddPortRequestPort struct {
+	NetworkID string                   `json:"network_id"`
+	FixedIps  []AddPortRequestFixedIps `json:"fixed_ips"`
+}
+type AddPortResponse struct {
+	Port *Port `json:"port"`
+}
+
+func (c *Conoha) AddPort(networkID string) (*Port, error) {
+	data := req.BodyJSON(AddPortRequest{AddPortRequestPort{NetworkID: networkID, FixedIps: []AddPortRequestFixedIps{}}})
+	r, err := req.Get(c.endPoint.ToUrl(NetworkService), data)
+	if err != nil {
+		return nil, err
+	}
+	if r.Response().StatusCode != 201 {
+		return nil, xerrors.Errorf("wrong status code: %v, message: %v", r.Response().StatusCode, r.String())
+	}
+	res := AddPortResponse{}
+	err = r.ToJSON(&res)
+	if err != nil {
+		return nil, err
+	}
+	return res.Port, nil
+}
