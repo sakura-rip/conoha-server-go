3e9190941c4697978b0b009934727199d0ae9a1a
sakura-rip <56917703+sakura-rip@users.noreply.github.com>
2021-05-23 10:34:07 +0900

    add: AddSubnetForAdditionalIp func

--- subnet.go
+++ subnet.go
@@ -43,3 +43,38 @@ func (c *Conoha) GetSubnetList() ([]Subnet, error) {
 	}
 	return res.Subnets, nil
 }
+
+type AddSubnetForAdditionalIpResponse struct {
+	Subnet *Subnet `json:"subnet"`
+}
+type AddSubnetForAdditionalIpRequest struct {
+	AllocateIp struct {
+		Bitmask string `json:"bitmask"`
+	} `json:"allocateip"`
+}
+
+// AddSubnetForAdditionalIp
+// /32 =>1個
+// /31 =>2個
+// /30 =>4個
+// /29 =>8個
+// /28 =>16個
+func (c *Conoha) AddSubnetForAdditionalIp(count string) (*Subnet, error) {
+	data := req.BodyJSON(AddSubnetForAdditionalIpRequest{AllocateIp: struct {
+		Bitmask string `json:"bitmask"`
+	}(struct{ Bitmask string }{Bitmask: count})})
+
+	r, err := req.Post(c.endPoint.ToUrl(NetworkService, "allocateips"), data)
+	if err != nil {
+		return nil, err
+	}
+	if r.Response().StatusCode != 201 {
+		return nil, xerrors.Errorf("wrong status code: %v, message: %v", r.Response().StatusCode, r.String())
+	}
+	res := AddSubnetForAdditionalIpResponse{}
+	err = r.ToJSON(&res)
+	if err != nil {
+		return nil, err
+	}
+	return res.Subnet, nil
+}
